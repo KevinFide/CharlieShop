c4f8fa62ddbc721d4983d14bd3b1f413ea1dd6cd
KevinFide <khidalgor420@gmail.com>
2026-08-09 02:04:22 -0600

    Add initial MVC interface and modules

---
+++ CharlieShop/Controllers/ReportesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CharlieShop.Controllers
+{
+    public class ReportesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
